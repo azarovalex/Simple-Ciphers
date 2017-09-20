345a9a28676428002de7d91d665eb82ec8391a55
azarovalex <alex@azarov.by>
2017-09-21 02:38:18 +0300

    Columns cipher encoding works nice.

--- columns.c
+++ columns.c
@@ -7,14 +7,72 @@
 //
 #include <stdlib.h>
 #include <string.h>
+#include <printf.h>
 
+static int arr[100];
+static char collumns[5000];
 
 const char *ColumnEncipher(const char *plaintext, const char *keyword) {
     int keywordLen = (int) strlen(keyword);
+    int plaintextLen = (int) strlen(plaintext);
+
+    int sizeX = plaintextLen / keywordLen;
+    if (plaintextLen % keywordLen)
+        sizeX += 1;
     
-    int arr[keywordLen];
-    int i = 1;
+    char matrix[sizeX][keywordLen];
+    
+    for (int i = 0, k = 0; i < sizeX; i++) {
+        for (int j = 0; j < keywordLen; j++) {
+            if (plaintext[k] == '\0') {
+                matrix[i][j] = '_';
+            } else {
+                matrix[i][j] = plaintext[k];
+            }
+            k++;
+        }
+    }
+    
+    char temp;
+    int  pos;
+    
+    for (int i = 0; i < keywordLen; i++)
+        arr[i] = -1;
+    
+    for (int i = 0; i < keywordLen; i++) {
+        temp = '~';
+        pos = 0;
+        
+        for (int j = 0; j < keywordLen; j++) {
+            if (temp > keyword[j] && arr[j] == -1) {
+                temp = keyword[j];
+                pos = j;
+            }
+            
+        }
+        arr[pos] = i;
+    }
+    
+    char *ciphertext = malloc(plaintextLen + 1);
+    int index = 0;
+    
+    for (int i = 0; i < keywordLen; i++) {
+        int j;
+        for (j = 0; j < keywordLen; j++) {
+            if (arr[j] == i) {
+                break;
+            }
+        }
+        for (int k = 0; k < sizeX; k++) {
+            if (matrix[k][j] != '_') {
+                ciphertext[index++] = matrix[k][j];
+            }
+        }
+    }
+    ciphertext[index] = '\0';
+
+
+    return ciphertext;
     
     
-    return "Hello, World!";
 }
